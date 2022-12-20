f680d797978348e047b19467183be5133cfebc10
Mehdi Najafi <dev.mehdinajafi@gmail.com>
2022-12-20 13:06:08 +0330

    chore: config svgr & tailwindcss in storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,19 +1,46 @@
-const path = require('path');
+const path = require("path");
+
 module.exports = {
-  "stories": [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../components/**/*.stories.{js,ts,jsx,tsx}"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    {
+      name: "@storybook/addon-postcss",
+      options: {
+        cssLoaderOptions: {
+          importLoaders: 1,
+        },
+        postcssLoaderOptions: {
+          implementation: require("postcss"),
+        },
+      },
+    },
   ],
-  "framework": {
-    "name": "@storybook/nextjs",
-    "options": {}
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
   },
-  "docs": {
-    "docsPage": true
-  }
-}
+  docs: {
+    docsPage: true,
+  },
+  webpackFinal: async (config) => {
+    config.resolve.alias = {
+      "@/": path.resolve(__dirname, "../"),
+    };
+
+    const svgLoaderRule = config.module.rules.find(
+      (rule) => rule.test && rule.test.test(".svg")
+    );
+    svgLoaderRule.exclude = /\.svg$/;
+
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+};
