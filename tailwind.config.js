cb960c0038b8de79273ea4c980d5d6c1496ff330
Mehdi Najafi <dev.mehdinajafi@gmail.com>
2022-12-20 13:21:47 +0330

    chore: extend tailwindcss theme config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,26 @@ module.exports = {
   ],
   theme: {
     extend: {
+      backgroundColor: {
+        background: "rgba(11, 29, 38, 1)",
+      },
+      fontSize: {
+        "5xl": ["3rem", "1.3125"],
+      },
       fontFamily: {
         sans: "'Poppins', sans-serif",
         serif: "'Roboto Slab', serif",
+      },
+      zIndex: {
+        fab: 1050,
+        drawer: 1200,
+        modal: 1300,
+        snackbar: 1400,
+        tooltip: 1500,
+      },
+      backgroundImage: {
+        "linear-gradient-1":
+          "linear-gradient(180deg, rgba(11, 29, 38, 0) 58.85%, #0B1D26 100%)",
       },
     },
   },
