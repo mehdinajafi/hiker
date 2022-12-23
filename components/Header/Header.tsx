4536e0d6e7925259c20e2ca9194f04cd3606cb97
Mehdi Najafi <dev.mehdinajafi@gmail.com>
2022-12-23 22:18:23 +0330

    component: Header

---
+++ components/Header/Header.tsx
@@ -0,0 +1,65 @@
+import Image from "next/image";
+import clsx from "clsx";
+import Navigation from "@/components/Navigation";
+import TwitterIcon from "@/public/icons/twitter.svg";
+import InstagramIcon from "@/public/icons/instagram.svg";
+import ArrowDownIcon from "@/public/icons/arrow_down.svg";
+
+const Header: React.FC = () => {
+  return (
+    <header className="relative">
+      <div className="relative h-[762px]">
+        <Image
+          src="/images/header-bg.jpg"
+          width={1347}
+          height={762}
+          alt="header"
+          className="absolute inset-0 h-full w-full object-cover object-[-100px] md:object-top"
+          loading="lazy"
+        />
+      </div>
+      <div className="absolute inset-0 bg-linear-gradient-1" />
+
+      <div className="absolute inset-x-0 top-0 px-8 pt-8">
+        <Navigation />
+
+        <div className="relative mt-32 flex items-center justify-center">
+          <div
+            className={clsx(
+              "left-0 mr-8 hidden items-center space-y-8 [writing-mode:vertical-lr]",
+              "md:static md:flex md:flex-grow md:items-start lg:absolute lg:flex-grow-0"
+            )}
+          >
+            <span className="text-lg text-white">Follow us</span>
+            <a href="">
+              <TwitterIcon />
+            </a>
+            <a href="">
+              <InstagramIcon />
+            </a>
+          </div>
+
+          <div className="flex max-w-[710px] flex-col">
+            <div
+              className={clsx(
+                "flex items-center text-sm font-extrabold tracking-[0.375rem] text-yellow-400 md:text-base",
+                "before:mr-3 before:h-px before:w-6 before:flex-shrink-0 before:bg-yellow-400 before:content-['_']"
+              )}
+            >
+              A Hiking Guide
+            </div>
+            <h1 className="mt-6 font-serif text-3xl font-extrabold text-white md:text-5xl">
+              Be prepared for the Mountains and beyond!
+            </h1>
+            <div className="mt-6 flex items-center space-x-8 font-bold text-white">
+              <span className="text-base md:text-lg">scroll down</span>
+              <ArrowDownIcon />
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
