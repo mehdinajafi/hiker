2b2adc1fbea7ebdc42141efe4fdc9cfa2c369fa7
Mehdi Najafi <dev.mehdinajafi@gmail.com>
2022-12-23 12:14:40 +0330

    component: Logo

---
+++ components/Logo/Logo.stories.tsx
@@ -0,0 +1,13 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Logo from ".";
+
+const meta: Meta<typeof Logo> = {
+  title: "Logo",
+  component: Logo,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Logo>;
+
+export const Default: Story = {};
---
+++ components/Logo/Logo.tsx
@@ -0,0 +1,7 @@
+const Logo = () => {
+  return (
+    <div className="font-serif text-3xl tracking-widest text-white">MNTN</div>
+  );
+};
+
+export default Logo;
