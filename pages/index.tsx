9dc9d3754cf3f88b8a56aea63bc68f9aaa4fc7c2
Mehdi Najafi <dev.mehdinajafi@gmail.com>
2023-01-14 11:59:57 +0330

    feat: get posts from api in home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,26 @@
+import { GetServerSideProps } from "next";
 import Head from "next/head";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import PostCard from "@/components/PostCard";
-import { NextPage } from "@/interfaces";
+import getPosts from "@/lib/api/blog/getPosts";
+import { IPost, NextPage } from "@/interfaces";
 
-const post = {
-  time: 1671957358231,
-  title: "What level of  hiker are you?",
-  description:
-    "Determining what level of hiker you are can be an important tool when planning future hikes....",
-  image: "/images/header-bg.jpg",
-  imageAlt: "Mountaineering runs on the mountain.",
-  slug: "what-level-of-hiker-are-you-?",
+interface IHomePage {
+  posts: IPost[];
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const posts = await getPosts();
+
+  return {
+    props: {
+      posts: JSON.parse(JSON.stringify(posts)),
+    },
+  };
 };
 
-const HomePage: NextPage = () => {
+const HomePage: NextPage<IHomePage> = (props) => {
   return (
     <>
       <Head>
@@ -25,15 +31,11 @@ const HomePage: NextPage = () => {
         <section className="mt-16">
           <h3 className="font-serif text-2xl font-semibold">Our Blog</h3>
           <div className="mt-6 grid grid-cols-12 gap-5">
-            <div className="col-span-12 md:col-span-4">
-              <PostCard {...post} />
-            </div>
-            <div className="col-span-12 md:col-span-4">
-              <PostCard {...post} />
-            </div>
-            <div className="col-span-12 md:col-span-4">
-              <PostCard {...post} />
-            </div>
+            {props.posts.map((post) => (
+              <div key={post._id} className="col-span-12 md:col-span-4">
+                <PostCard {...post} />
+              </div>
+            ))}
           </div>
         </section>
       </main>
