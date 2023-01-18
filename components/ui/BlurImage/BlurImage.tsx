05aad5a960f8e032a1bbeec8f1ed787b0bf21509
Mehdi Najafi <dev.mehdinajafi@gmail.com>
2023-01-18 12:36:41 +0330

    component: BlurImage

---
+++ components/ui/BlurImage/BlurImage.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import Image, { ImageProps } from "next/image";
+import clsx from "clsx";
+
+type IBlurImage = Omit<ImageProps, "onLoadingComplete">;
+
+const BlurImage: React.FC<IBlurImage> = (props) => {
+  const [isLoading, setLoading] = useState(true);
+
+  return (
+    <Image
+      {...props}
+      src={props.src}
+      alt={props.alt}
+      width={props.width}
+      height={props.height}
+      className={clsx(
+        "transition-[filter] duration-700 ease-in-out",
+        {
+          "animate-pulse rounded-md bg-gray-200 blur-sm ": isLoading,
+          "blur-0": !isLoading,
+        },
+        props.className
+      )}
+      onLoadingComplete={() => setLoading(false)}
+    />
+  );
+};
+
+export default BlurImage;
