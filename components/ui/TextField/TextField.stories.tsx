3c401fc176bd17a67d5c437896c4029353b7b787
Mehdi Najafi <dev.mehdinajafi@gmail.com>
2023-01-27 16:31:01 +0330

    story: fix TextField typo and add label to it

--- components/ui/TextField/TextField.stories.tsx
+++ components/ui/TextField/TextField.stories.tsx
@@ -1,13 +1,17 @@
-import { Meta, StoryFn, StoryObj } from "@storybook/react";
+import { Meta, StoryObj } from "@storybook/react";
 import TextField from ".";
 
 type TextFieldType = typeof TextField;
 
 const meta: Meta<TextFieldType> = {
-  title: "Design Design/TextField",
+  title: "Design System/TextField",
   component: TextField,
 };
 
 export default meta;
 
-export const Default: StoryObj<TextFieldType> = {};
+export const Default: StoryObj<TextFieldType> = {
+  args: {
+    label: "Label",
+  },
+};
